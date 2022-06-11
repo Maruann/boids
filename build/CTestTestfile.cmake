27049194940d7cdf1698ce1336598adfbbb1b38f
Francesco Tori <francesco.tori@studio.unibo.it>
2022-06-11 18:52:52 +0200

    cartella build eliminata

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/maruan/project_boids
-# Build directory: /home/maruan/project_boids/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(simulation.t "/home/maruan/project_boids/build/simulation.t")
-set_tests_properties(simulation.t PROPERTIES  _BACKTRACE_TRIPLES "/home/maruan/project_boids/CMakeLists.txt;28;add_test;/home/maruan/project_boids/CMakeLists.txt;0;")
