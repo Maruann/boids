b0762335f0cf747bc45e3a45afd2a79674038f20
Lorenzo Mariotti <lorenzomariotti60@gmail.com>
2022-06-11 11:16:15 +0200

    rimossa la cartella non utile

--- chain_sfml_cmake/ppstate.cpp
+++
@@ -1,8 +0,0 @@
-#include "ppstate.hpp"
-
-#include <iostream>
-
-std::ostream& operator<<(std::ostream& os, PPState const& p) {
-  os << "( m: " << p.m << ", x: " << p.x << ", v: " << p.v << ')';
-  return os;
-}
