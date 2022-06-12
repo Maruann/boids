13cbf3847933fa2e68e561a068f301fb7f01fd33
Lorenzo Mariotti <lorenzomariotti60@gmail.com>
2022-06-12 08:40:09 +0200

    aggiunta la repulsione sulle x, inizio il testing

--- flock.hpp
+++ flock.hpp
@@ -45,14 +45,25 @@ class Flock {
 
   void upt_xc_() {
     xc_ = std::accumulate(
-        flock.begin(), flock.end(), 0.,
-        [&](double sum, Boid boid) { return sum + boid.get_x(); })/flock.size();
+              flock.begin(), flock.end(), 0.,
+              [](double sum, Boid boid) { return sum + boid.get_x(); }) /
+          flock.size();
   }
   void upt_yc_() {
     yc_ = std::accumulate(
-        flock.begin(), flock.end(), 0.,
-        [&](double sum, Boid boid) { return sum + boid.get_y(); })/flock.size();
+              flock.begin(), flock.end(), 0.,
+              [](double sum, Boid boid) { return sum + boid.get_y(); }) /
+          flock.size();
   }
+
+  double vx_repulsive(double dx_s, int i) {   //prende in entrata dx_x che è la distanza minima sulle x per avere una repulsione e i che è il numero del boid che si vuole fissare
+    auto lambda = [&](double sum, Boid boid) {      //lambda che fa da operatore binario all'accumulate, prende in entrata sum ed il boid sul quale
+      return (fabs(boid.get_x() - flock[i].get_x()) > dx_s)   //è arrivato l'iteratore, mentre cattura by reference flock e ne prende l'i-esimo boid
+                 ? sum + boid.get_x() - flock[i].get_x()      //ne fa la differenza sulle x e: se in modulo è minore di dx_s ne returna il termine della sommatoria
+                 : 0;                                         //se è maggiore returna 0
+    };
+    return -sep_ * std::accumulate(flock.begin(), flock.end(), 0, lambda);
+  } //questo accumulate somma le differenze sulle x tra tutti i boids e quello fissato da i
 };
 
 /*  LASCIO QUI IL METODO NEL CASO SI DECIDESSE DI USARE UN ARRAY
--- simulation.test.cpp
+++ simulation.test.cpp
@@ -1,3 +1,5 @@
-int main(){
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include "flock.hpp"
+#include "doctest.h"
 
-}
+TEST_CASE("Test riempimento flock"){};
