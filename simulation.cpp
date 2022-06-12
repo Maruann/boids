043638ba60871c59a3499ff0ee6397595084b2c3
Francesco Tori <francesco.tori@studio.unibo.it>
2022-06-12 22:38:32 +0200

    risolto bug, boids immobili (ho messo n=1)

---
+++ simulation.cpp
@@ -0,0 +1,57 @@
+#include "flock.hpp"
+#include <SFML/Graphics.hpp>
+#include <SFML/System/Time.hpp>
+
+auto evolve(Flock& flock, int steps_per_evolution, sf::Time delta_t){
+    double const dt{delta_t.asSeconds()};
+
+    for(int s{0}; s != steps_per_evolution; ++s){
+        evolve(flock,dt);
+    }
+
+    return flock.get_flock(); 
+}
+
+int main(){
+    Flock stormo{0.5, 0.5, 0.5};
+    stormo.fill(1);
+
+    auto const delta_t{sf::milliseconds(1)};
+    int const fps = 30;
+    int const steps_per_evolution{1000 / fps};
+
+
+    unsigned const int display_width = 100;
+    unsigned const int display_height = 100;
+
+
+    sf::RenderWindow window(sf::VideoMode(display_width, display_height), "BOOOIDZZZZZ");
+    
+    window.setFramerateLimit(fps);
+
+    sf::CircleShape circ{5.0f};
+    circ.setFillColor(sf::Color::Blue);
+
+    while (window.isOpen()){
+        sf::Event event;
+        while (window.pollEvent(event)){
+            if (event.type == sf::Event::Closed){
+                window.close();
+            }
+        }
+
+        window.clear(sf::Color::White);
+
+
+        auto const state = evolve(stormo, steps_per_evolution, delta_t);
+
+        for (auto& boid : state){
+            circ.setPosition(boid.get_x(), boid.get_y());
+
+            window.draw(circ); //disegna sull' oggetto window, ma non la "displaya" ancora
+        }
+
+     window.display();   //adesso display
+    }
+
+}
--- simulation.test.cpp
+++ simulation.test.cpp
@@ -1,1 +1,33 @@
-int main(){}
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include <iostream>
+
+#include "doctest.h"
+#include "flock.hpp"
+
+TEST_CASE("Test riempimento flock") {
+  Flock flock{1, 1, 1};
+  flock.fill(10);
+
+ /* std::cout << flock.get_flock().size() << '\n';  // WE GOOD BOIDDDDSSS
+  CHECK(flock.get_flock().size() == 10);
+  for (Boid boid : flock.get_flock()) {
+    std::cout << boid.get_x() << "  " << boid.get_y() << '\n';
+  }
+  std::cout << "fine test 1" << '\n';
+*/
+  std::cout << flock.vx_repulsive(1000., 0)
+            << '\n';  // stampa la velocità repulsiva sul singolo boid
+                      // considerando tutti quelli presenti
+  for (Boid boid : flock.get_flock()) {
+    std::cout << boid.get_x() << "  " << boid.get_y()
+              << '\n';  // stampa le posizioni dei singoli boids per un
+                        // calcolo a manina
+  }
+  std::cout << flock.vx_alignment(0) << '\n';
+  for (Boid boid : flock.get_flock()) {
+    std::cout << boid.get_vx() << "  " << boid.get_vy()
+              << '\n';  // stampa le velocità dei singoli boids per un
+                        // calcolo a manina
+  }
+  std::cout << flock.vx_coesion(0) << '\n';
+};
